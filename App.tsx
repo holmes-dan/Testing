a1313a05436559be9ac4bda11008d0f8bf9881a0
holmes-dan <holmes.dan@proton.me>
2025-04-09 10:58:35 -0400

    Commit to test ci.yml

--- App.tsx
+++ App.tsx
@@ -85,8 +85,6 @@ function App(): React.JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
           <LearnMoreLinks />
         </View>
       </ScrollView>
